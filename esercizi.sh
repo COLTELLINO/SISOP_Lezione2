9360927a406336e3419c9d7751ee4b1e341cad67
Filippo Patrignani <filippo.patrignani2@studio.unibo.it>
2024-12-04 15:51:37 +0100

    Update esercizi.sh

--- esercizi.sh
+++ esercizi.sh
@@ -1,21 +1,21 @@
-echo ${USER} ${HOME} ${DISPLAY}
-env|more
-PIPPO=${USER}${HOME}${DISPLAY}
-source crea_var.sh
+echo ${USER} ${HOME} ${DISPLAY} ;
+env|more ;
+PIPPO=${USER}${HOME}${DISPLAY} ;
+source crea_var.sh ;
+ 
+cd / ; echo ${HOME} ;
+mkdir A1 ; mkdir A1/B2 ; mkdir A1/B2/C3 ; for num in 1 2 3 ; do touch A1/B2/C3/${num}.txt ; done ; mv A1/B2/C3/*.txt A1/B2 ; cp A1/B2/*.txt A1 ;
+cat .* ;
+ls /usr/include/linux/netfilter/nf_nat.h ;
+history ; !10 ;
+set +o history ;
+set -a ; env|more ;
 
-cd / ; echo ${HOME}
-mkdir A1 ; mkdir A1/B2 ; mkdir A1/B2/C3 ; for num in 1 2 3 ; do touch A1/B2/C3/${num}.txt ; done ; mv A1/B2/C3/*.txt A1/B2 ; cp A1/B2/*.txt A1 
-cat .*
-ls /usr/include/linux/netfilter/nf_nat.h
-history ; !10
-set +o history
-set -a ; env|more
-
-ls -al /usr/lib/*plu*
-man ls
-ls -alR /usr/include
-ls -dl /usr/include
-mkdir BUTTAMI ; touch BUTTAMI/ABC.txt ; touch BUTTAMI/AbC.txt
-rm BUTTAMI/ABC.txt
-rm -r -f BUTTAMI
+ls -al /usr/lib/*plu* ;
+man ls ;
+ls -alR /usr/include ;
+ls -dl /usr/include ;
+mkdir BUTTAMI ; touch BUTTAMI/ABC.txt ; touch BUTTAMI/AbC.txt ;
+rm BUTTAMI/ABC.txt ;
+rm -r -f BUTTAMI ;
 ls -d /usr/lib/*[1-3c-m]*.0
